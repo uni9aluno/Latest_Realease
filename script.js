b6f33522d95a76183717a68cbe0ca58fda30e210
Gabriel Lunaro de Oliveira <136655304+uni9aluno@users.noreply.github.com>
2024-11-22 01:17:15 -0300

    printscreen

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const ocultarTraducaoBtn = document.getElementById('ocultar-traducao');
 const textoFixo = document.getElementById('texto-fixo');
 
 // Adiciona um ouvinte de evento para o botão "Ocultar Tradução"
-ocultarTraducaoBtn.addEventListener('click', function() {
+ocultarTraducaoBtn.addEventListener('click', function () {
   // Alterna a visibilidade do texto
   if (textoFixo.style.display === 'none') {
     textoFixo.style.display = 'block'; // ou o estilo de exibição original
@@ -28,14 +28,14 @@ ocultarTraducaoBtn.addEventListener('click', function() {
 });
 
 function updatePreview() {
-    // Atualizar cada bloco separadamente
-    periodoPreviewNumeral.textContent = periodonumeral.value;
-    periodoPreview.textContent = periodo.value;
-    nomeFalecidoPreview.textContent = nomeFalecido.value;
-    nomeFalecidoJaponesPreview.textContent = nomeFalecidoJapones.value;
-    nomeFamiliaPreview.textContent = nomeFamilia.value;
-    nomeFamiliaJaponesPreview.textContent = nomeFamiliaJapones.value;
-    relacaoPreview.textContent = relacao.value;
+  // Atualizar cada bloco separadamente
+  periodoPreviewNumeral.textContent = periodonumeral.value;
+  periodoPreview.textContent = periodo.value;
+  nomeFalecidoPreview.textContent = nomeFalecido.value;
+  nomeFalecidoJaponesPreview.textContent = nomeFalecidoJapones.value;
+  nomeFamiliaPreview.textContent = nomeFamilia.value;
+  nomeFamiliaJaponesPreview.textContent = nomeFamiliaJapones.value;
+  relacaoPreview.textContent = relacao.value;
 }
 
 // Adiciona ouvintes de evento para cada campo
@@ -47,56 +47,81 @@ nomeFamilia.addEventListener('input', updatePreview);
 nomeFamiliaJapones.addEventListener('input', updatePreview);
 periodo.addEventListener('change', updatePreview);
 
-// Função de arrastar e obter coordenadas
- 
+// Função para tirar print
+document.getElementById('tirar-print').addEventListener('click', function () {
+  const previewElement = document.getElementById('preview');
+
+  
+  domtoimage
+    .toBlob(previewElement, {
+      style: {
+        'background-color': '#ffffff', // Garante um fundo branco na imagem gerada
+      },
+    })
+    .then((blob) => {
+      const link = document.createElement('a');
+      link.download = 'preview.png';
+      link.href = URL.createObjectURL(blob);
+      link.click();
+    })
+    .catch((error) => {
+      console.error('Erro ao capturar o print:', error);
+    });
+});
 
 
-/// Função para baixar o PDF
-document.getElementById('download-pdf').addEventListener('click', function() {
-    const { jsPDF } = window.jspdf;
-    const img = new Image();
-    img.src = 'https://i.ibb.co/7XXLP89/Noshigami-Okaeshi-LOGO-NOVO-36-5cm-x-16cm.jpg';
+document.fonts.ready.then(() => {
+    console.log("Todas as fontes foram carregadas com sucesso.");
+}).catch((error) => {
+    console.error("Erro ao carregar fontes:", error);
+});
 
-    img.onload = function() {
-        const imgWidthMM = 365; // Largura da imagem em milímetros
-        const imgHeightMM = 160; // Altura da imagem em milímetros
 
-        const doc = new jsPDF('l', 'mm', [imgWidthMM, imgHeightMM]);
-        doc.addImage(img, 'PNG', 0, 0, imgWidthMM, imgHeightMM);
+// Função para baixar o PDF
+document.getElementById('download-pdf').addEventListener('click', function () {
+  const { jsPDF } = window.jspdf;
+  const img = new Image();
+  img.src = 'https://i.ibb.co/7XXLP89/Noshigami-Okaeshi-LOGO-NOVO-36-5cm-x-16cm.jpg';
 
-        // Captura o texto fixo
-        const textoFixo = document.getElementById('texto-fixo').textContent.trim();
-        const periodoPreview = document.getElementById('periodoPreview').textContent.trim();
-        const periodoPreviewNumeral = document.getElementById('periodoPreviewNumeral').textContent.trim();
-        const nomeFalecidoPreview = document.getElementById('nomeFalecidoPreview').textContent.trim();
-        const nomeFamiliaPreview = document.getElementById('nomeFamiliaPreview').textContent.trim();
+  img.onload = function () {
+    const imgWidthMM = 365; // Largura da imagem em milímetros
+    const imgHeightMM = 160; // Altura da imagem em milímetros
 
-        // Verificar no console se os textos estão sendo capturados
-        console.log("Texto Fixo:", textoFixo);
-        console.log("Periodo Preview:", periodoPreview);
-        console.log("Periodo Numeral Preview:", periodoPreviewNumeral);
-        console.log("Nome Falecido Preview:", nomeFalecidoPreview);
-        console.log("Nome Familia Preview:", nomeFamiliaPreview);
+    const doc = new jsPDF('l', 'mm', [imgWidthMM, imgHeightMM]);
+    doc.addImage(img, 'PNG', 0, 0, imgWidthMM, imgHeightMM);
 
-        // Posições fixas para teste
-        const pdfX = 20; // X em milímetros
-        let pdfY = 30; // Y em milímetros, começando abaixo da imagem
+    // Captura o texto fixo
+    const textoFixo = document.getElementById('texto-fixo').textContent.trim();
+    const periodoPreview = document.getElementById('periodoPreview').textContent.trim();
+    const periodoPreviewNumeral = document.getElementById('periodoPreviewNumeral').textContent.trim();
+    const nomeFalecidoPreview = document.getElementById('nomeFalecidoPreview').textContent.trim();
+    const nomeFamiliaPreview = document.getElementById('nomeFamiliaPreview').textContent.trim();
 
-        // Configura o tamanho da fonte
-        doc.setFontSize(16);
-        doc.setTextColor(0, 0, 0); // Cor preta
+    // Verificar no console se os textos estão sendo capturados
+    console.log('Texto Fixo:', textoFixo);
+    console.log('Periodo Preview:', periodoPreview);
+    console.log('Periodo Numeral Preview:', periodoPreviewNumeral);
+    console.log('Nome Falecido Preview:', nomeFalecidoPreview);
+    console.log('Nome Familia Preview:', nomeFamiliaPreview);
 
-        // Adiciona o texto ao PDF
-        doc.text(textoFixo, pdfX, pdfY);
-        pdfY += 10; // Ajustar a posição Y para o próximo texto
-        doc.text(`${periodoPreview} ${periodoPreviewNumeral}`, pdfX, pdfY);
-        pdfY += 10; // Ajustar a posição Y
-        doc.text(`${nomeFalecidoPreview}`, pdfX, pdfY);
-        pdfY += 10; // Ajustar a posição Y
-        doc.text(`${nomeFamiliaPreview}`, pdfX, pdfY);
-        
+    // Posições fixas para teste
+    const pdfX = 20; // X em milímetros
+    let pdfY = 30; // Y em milímetros, começando abaixo da imagem
 
-        // Salva o PDF com o nome desejado
-        doc.save('Noshigami.pdf');
-    };
+    // Configura o tamanho da fonte
+    doc.setFontSize(16);
+    doc.setTextColor(0, 0, 0); // Cor preta
+
+    // Adiciona o texto ao PDF
+    doc.text(textoFixo, pdfX, pdfY);
+    pdfY += 10; // Ajustar a posição Y para o próximo texto
+    doc.text(`${periodoPreview} ${periodoPreviewNumeral}`, pdfX, pdfY);
+    pdfY += 10; // Ajustar a posição Y
+    doc.text(`${nomeFalecidoPreview}`, pdfX, pdfY);
+    pdfY += 10; // Ajustar a posição Y
+    doc.text(`${nomeFamiliaPreview}`, pdfX, pdfY);
+
+    // Salva o PDF com o nome desejado
+    doc.save('Noshigami.pdf');
+  };
 });
